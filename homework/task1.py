46b3d62cddfb1ed6aa0633a8e2f0ea5e8c770fd0
Ilfat Nigmatullin <zilibob1337@gmail.com>
2023-05-05 21:35:25 +0500

    Выполнено

--- homework/task1.py
+++ homework/task1.py
@@ -26,3 +26,4 @@ for i, d in enumerate(data):
     assert modification(d) == test_data[i], f'С набором {d} есть ошибка, не проходит проверку'
     print(f'Тестовый набор {d} прошёл проверку')
 print('Всё ок')
+
