02ae1c1b759e1fb7b38f77dcfe93f6afe85fbab4
Ilfat Nigmatullin <zilibob1337@gmail.com>
2023-05-23 01:23:13 +0500

    Шестое задание - ок

---
+++ Work6/task1.py
@@ -0,0 +1,28 @@
+# Глобальные перемены.
+# Имеется следующие переменные, определенные в глобальной области видимости модуля:
+# number = 1
+# string = 'Hello'
+# Напишите и вызовите функцию, которая будет изменять и возвращать эти переменные, на следующие значения:
+# number = 5
+# string = 'Hello, dear friend'
+#
+
+number = 1
+string = 'Hello'
+
+
+def global_changes():
+    global number, string
+    number, string = 5, 'Hello, dear friend'
+    return number, string
+
+
+global_changes()
+
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+assert number == 5, 'Переменная number должна иметь значение 5'
+assert string == 'Hello, dear friend', 'Переменная number должна иметь значение Home, sweet home'
+assert global_changes() == (5, 'Hello, dear friend')
+
+print('Все ок')
