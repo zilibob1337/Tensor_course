244c10a6114de2d0e003c063d7b16eff3f7af5c0
Ilfat Nigmatullin <zilibob1337@gmail.com>
2023-05-30 00:26:10 +0500

    Седьмое задание

---
+++ work7/task1.py
@@ -0,0 +1,55 @@
+# Напишите класс Segment
+# Для его инициализации нужно два кортежа с координатами точек (x1, y1), (x2, y2)
+# Реализуйте методы класса:
+# 1. length, который возвращает длину нашего отрезка, с округлением до 2 знаков после запятой
+# 2. x_axis_intersection, который возвращает True, если отрезок пересекает ось абцисс, иначе False
+# 3. y_axis_intersection, который возвращает True, если отрезок пересекает ось ординат, иначе False
+# Например (Ввод --> Вывод) :
+# Segment((2, 3), (4, 5)).length() --> 2.83
+# Segment((-2, -3), (4, 5)).x_axis_intersection() --> True
+# Segment((-2, -3), (-4, -5)).y_axis_intersection() --> False
+
+class Segment:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def length(self):
+        x1, y1 = self.point1
+        x2, y2 = self.point2
+        distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+        return round(distance, 2)
+
+    def x_axis_intersection(self):
+        y1 = self.point1[1]
+        y2 = self.point2[1]
+        return y1 * y2 <= 0
+
+    def y_axis_intersection(self):
+        x1 = self.point1[0]
+        x2 = self.point2[0]
+        return x1 * x2 <= 0
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+data = [Segment((2, 3), (4, 5)).length,
+        Segment((1, 1), (1, 8)).length,
+        Segment((0, 0), (0, 1)).length,
+        Segment((15, 1), (18, 8)).length,
+        Segment((-2, -3), (4, 5)).x_axis_intersection,
+        Segment((-2, -3), (-4, -2)).x_axis_intersection,
+        Segment((0, -3), (4, 5)).x_axis_intersection,
+        Segment((2, 3), (4, 5)).y_axis_intersection,
+        Segment((-2, -3), (4, 5)).y_axis_intersection,
+        Segment((-2, 3), (4, 0)).y_axis_intersection
+        ]
+
+
+test_data = [2.83, 7.0, 1.0, 7.62, True, False, True, False, True, True]
+
+for i, d in enumerate(data):
+    assert_error = f'Не прошла проверка для метода {d.__qualname__} экземпляра с атрибутами {d.__self__.__dict__}'
+    assert d() == test_data[i], assert_error
+    print(f'Набор для метода {d.__qualname__} экземпляра класса с атрибутами {d.__self__.__dict__} прошёл проверку')
+print('Всё ок')
---
+++ work7/task3.py
@@ -0,0 +1,103 @@
+# I. Напишите класс PublicTransport
+# Экземпляр класса создается из следующих атрибутов:
+#   1. brand - Марка транспорта
+#   2. ЗАЩИЩЕННЫЙ (protected) атрибут engine_power - Мощность двигателя
+#   3. year - Год выпуска
+#   4. color - Цвет
+#   5. max_speed - Максимальная скорость
+# У класса должно быть СВОЙСТВО info, которое выводит на печать информацию о:
+# марке, цвете, годе выпуска и мощности двигателя
+#
+# II. Напишите класс Bus унаследованный от PublicTransport
+# Дополнительными атрибутами будут:
+#   1. passengers - кол-во пассажиров
+#   2. ПРИВАТНЫЙ (private) атрибут park - Парк приписки автобуса
+#   3. ЗАЩИЩЕННЫЙ (protected) атрибут fare - Стоимость проезда
+# Добавить свойство park, которое будет возвращать значение park
+# а при присвоении проверять номер парка, что он в диапазоне от 1000 до 9999
+#
+# III. Напишите класс Tram унаследованный от PublicTransport
+# Дополнительными атрибутами будут:
+#   1. ПРИВАТНЫЙ (private) атрибут route - маршрут трамвая
+#   2. path - длина маршрута
+#   3. ЗАЩИЩЕННЫЙ (protected) атрибут fare - Стоимость проезда
+# У класса должно быть СВОЙСТВО how_long, которое вычисляет время прохождения маршрута по формуле max_speed/(4*path)
+
+# первое (класс PublicTransport)
+class PublicTransport:
+    def __init__(self, brand, engine_power, year, color, max_speed):
+        self.brand = brand
+        self._engine_power = engine_power
+        self.year = year
+        self.color = color
+        self.max_speed = max_speed
+
+    @property
+    def info(self):
+        return f'Марка: {self.brand}, Цвет: {self.color}, Год выпуска: {self.year}, Мощность двигателя:' \
+               f' {self._engine_power}'
+
+
+# второе (класс Bus унаследованный от PublicTransport)
+class Bus(PublicTransport):
+    def __init__(self, brand, engine_power, year, color, max_speed, passengers, park, fare):
+        super().__init__(brand, engine_power, year, color, max_speed)
+        self.passengers = passengers
+        self.park = park
+        self._fare = fare
+
+    @property
+    def park(self):
+        return self.__park
+
+    @park.setter
+    def park(self, value):
+        assert 1000 <= value <= 9999, 'Номер парка должен быть в диапазоне от 1000 до 9999'
+        self.__park = value
+
+
+# третье (класс Tram унаследованный от PublicTransport)
+class Tram(PublicTransport):
+    def __init__(self, brand, engine_power, year, color, max_speed, route, path, fare):
+        super().__init__(brand, engine_power, year, color, max_speed)
+        self.__route = route
+        self.path = path
+        self._fare = fare
+
+    @property
+    def how_long(self):
+        return self.max_speed / (4 * self.path)
+
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+transport = PublicTransport('Автомобиль', 500, 2040, 'Фиолетовый', 300)
+first_bus = Bus('ЛиАЗ', 210, 2015, 'Зеленый', 100, 70, 1232, 32)
+second_bus = Bus('VOLGABUS', 320, 2019, 'Желтый', 110, 39, 1111, 32)
+first_tram = Tram('71-931M', 125, 2010, 'Красный', 75, 5, 15, 32)
+second_tram = Tram('71-409-1', 240, 2018, 'Белый', 85, 7, 17, 32)
+
+assert isinstance(type(transport).info, property), 'В классе PublicTransport, info - не свойство класса'
+assert transport._engine_power, 'В классе PublicTransport, engine_power не защищенный атрибут'
+assert first_bus._Bus__park, 'В классе Bus, park не приватный атрибут'
+assert second_bus._fare, 'В классе Bus, fare не защищенный атрибут'
+assert first_tram._fare, 'В классе Tram, fare не защищенный атрибут'
+assert second_tram._Tram__route, 'В классе Tram, route не приватный атрибут'
+assert isinstance(type(first_tram).how_long, property), 'В классе Tram, how_long - не свойство класса'
+assert first_tram.how_long == 1.25, 'В классе Tram, how_long неверно вычисляется'
+assert isinstance(type(second_bus).park, property), 'В классе Bus, park - не свойство класса'
+try:
+    second_bus.park = 999
+    raise Exception('Проверка на ограничение диапазона НЕ пройдена')
+except AssertionError:
+    print('Проверка на правильность диапазона пройдена')
+try:
+    second_bus.park = 1234
+    print('Проверка на правильность диапазона пройдена')
+except AssertionError:
+    raise Exception('Проверка на ограничение диапазона НЕ пройдена')
+try:
+    second_bus.park = 10000
+    raise Exception('Проверка на ограничение диапазона НЕ пройдена')
+except AssertionError:
+    print('Проверка на правильность диапазона пройдена')
+print('Всё ок')
